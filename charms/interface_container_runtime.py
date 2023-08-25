5ebff3d54df6e5d917391a17f343792b604e8e20
George Kraft <george.kraft@canonical.com>
2023-08-25 14:05:36 -0500

    Initial implementation

---
+++ charms/interface_container_runtime.py
@@ -0,0 +1,19 @@
+class ContainerRuntimeProvides:
+    """Implements the Provides side of the container-runtime interface."""
+
+    def __init__(self, charm, endpoint):
+        self.charm = charm
+        self.endpoint = endpoint
+
+    @property
+    def relations(self):
+        return self.charm.model.relations[self.endpoint]
+
+    def set_sandbox_image(self, sandbox_image):
+        """Set the sandbox image."""
+        for relation in self.relations:
+            relation.data[self.unit]["sandbox_image"] = sandbox_image
+
+    @property
+    def unit(self):
+        return self.charm.unit
