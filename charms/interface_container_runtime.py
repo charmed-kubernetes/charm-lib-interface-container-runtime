097295a27c23b85e9606d2a419cee8e49e11071d
George Kraft <george.kraft@canonical.com>
2023-09-13 15:05:25 -0500

    Address PR feedback

--- charms/interface_container_runtime.py
+++ charms/interface_container_runtime.py
@@ -1,27 +1,30 @@
+from ops import CharmBase, Relations, Unit
+from typing import List, Optional
+
+
 class ContainerRuntimeProvides:
     """Implements the Provides side of the container-runtime interface."""
 
-    def __init__(self, charm, endpoint):
+    def __init__(self, charm: CharmBase, endpoint: str):
         self.charm = charm
         self.endpoint = endpoint
 
     @property
-    def relations(self):
+    def relations(self) -> List[Relations]:
         return self.charm.model.relations[self.endpoint]
 
-    def set_sandbox_image(self, sandbox_image):
+    def set_sandbox_image(self, sandbox_image: str) -> None:
         """Set the sandbox image."""
         for relation in self.relations:
             relation.data[self.unit]["sandbox_image"] = sandbox_image
 
     @property
-    def socket(self):
+    def socket(self) -> Optional[str]:
         for relation in self.relations:
             for unit in relation.units:
-                socket = relation.data[unit].get("socket")
-                if socket:
+                if socket := relation.data[unit].get("socket"):
                     return socket
 
     @property
-    def unit(self):
+    def unit(self) -> Unit:
         return self.charm.unit
