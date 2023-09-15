724cddb9d62612c2cb694a8bca4d8e61f404a353
George Kraft <george.kraft@canonical.com>
2023-09-15 11:58:07 -0500

    Initial implementation (#1)
    * Initial implementation
    
    * Add socket property
    
    * Address PR feedback
    
    * Fix broken import and type hint

---
+++ README.md
@@ -0,0 +1,3 @@
+# container-runtime Charm Interface Library
+
+Charm library for container-runtime interface.
---
+++ charms/interface_container_runtime.py
@@ -0,0 +1,30 @@
+from ops import CharmBase, Relation, Unit
+from typing import List, Optional
+
+
+class ContainerRuntimeProvides:
+    """Implements the Provides side of the container-runtime interface."""
+
+    def __init__(self, charm: CharmBase, endpoint: str):
+        self.charm = charm
+        self.endpoint = endpoint
+
+    @property
+    def relations(self) -> List[Relation]:
+        return self.charm.model.relations[self.endpoint]
+
+    def set_sandbox_image(self, sandbox_image: str) -> None:
+        """Set the sandbox image."""
+        for relation in self.relations:
+            relation.data[self.unit]["sandbox_image"] = sandbox_image
+
+    @property
+    def socket(self) -> Optional[str]:
+        for relation in self.relations:
+            for unit in relation.units:
+                if socket := relation.data[unit].get("socket"):
+                    return socket
+
+    @property
+    def unit(self) -> Unit:
+        return self.charm.unit
